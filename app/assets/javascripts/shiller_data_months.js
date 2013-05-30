ae05500d9770ba74fddd73641af2a7bd5e92c31f
MrPowers <matthewkevinpowers@gmail.com>
2013-05-29 22:12:28 -0400

    Refactored code to be more consistent with organization of documentation

--- app/assets/javascripts/shiller_data_months.js
+++ app/assets/javascripts/shiller_data_months.js
@@ -23,32 +23,39 @@ $(function() {
         hour: '%b %Y',
         day: '%b %Y',
         week: '%b %Y',
-        month: '%Y',
+        month: '%b %Y',
         year: '%Y'
+      }
+    },
+
+    plotOptions: {
+      series: {
+        dataGrouping: {
+          //enabled : false
+          forced : true,
+          //smoothed: true,
+          dateTimeLabelFormats: {
+            millisecond: ['%B %Y', '', '%B %Y'],
+            second: ['%B %Y', '', '%B %Y'],
+            minute: ['%B %Y', '', '%B %Y'],
+            hour: ['%B %Y', '', '%B %Y'],
+            day: ['%B %Y', '', '%B %Y'],
+            week: ['%B %Y', '', '%B %Y'],
+            month: ['%B %Y', '', '%B %Y'],
+            year: ['%Y', '%Y', '-%Y']
+          }
+        }
       }
     },
 
     series : [{
       name : 'CAPE',
-      data : $('#cape_data').data('result'),
-      tooltip: {
-        valueDecimals: 2,
-      },
-      dataGrouping: {
-        //enabled : false
-        forced : true,
-        smoothed: true,
-        dateTimeLabelFormats: {
-          millisecond: ['%B %Y', '', '%B %Y'],
-          second: ['%B %Y', '', '%B %Y'],
-          minute: ['%B %Y', '', '%B %Y'],
-          hour: ['%B %Y', '', '%B %Y'],
-          day: ['%B %Y', '', '%B %Y'],
-          week: ['%B %Y', '', '%B %Y'],
-          month: ['%B %Y', '', '%B %Y'],
-          year: ['%Y', '%Y', '-%Y']
-        }
-      }
-    }]//end series
-  });//end highcharts
+      data : $('#cape_data').data('result')
+    }],
+
+    tooltip: {
+      valueDecimals: 2,
+    }
+
+  });
 });
